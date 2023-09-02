93ae609973e0eb7c7d0f13332cfe4920d74907c2
Khubaib Jamal <khubaib.j18@gmail.com>
2023-09-03 00:20:57 +0500

    saving card

--- mini_project/lib/screen/home/component/body.dart
+++ mini_project/lib/screen/home/component/body.dart
@@ -46,25 +46,108 @@ class Body extends StatelessWidget {
                 vertical: getProportionateScreenWidth(15),
                 horizontal: getProportionateScreenWidth(15),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        HomeBanner(bgColor: AppColor.kSecondaryColor),
-                        HomeBanner(bgColor: Color(0xFFE4DDCB)),
-                      ],
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          HomeBanner(bgColor: AppColor.kSecondaryColor),
+                          HomeBanner(bgColor: Color(0xFFE4DDCB)),
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(height: getProportionateScreenWidth(20)),
-                  const PopularProducts(),
-                ],
+                    SizedBox(height: getProportionateScreenWidth(20)),
+                    const PopularProducts(),
+                    SizedBox(height: getProportionateScreenWidth(20)),
+                    const SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          SavingCart(
+                            itemText: "346",
+                            timeText: "USD",
+                            subText: "Your Total Saving",
+                            bgColor: AppColor.kSecondaryLightColor,
+                          ),
+                          SavingCart(
+                            itemText: "215",
+                            timeText: "HRS",
+                            subText: "Your Time Saving",
+                            bgColor: Color(0xFFE4DDCB),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class SavingCart extends StatelessWidget {
+  final String itemText, timeText, subText;
+  final Color bgColor;
+  const SavingCart({
+    super.key,
+    required this.itemText,
+    required this.timeText,
+    required this.subText,
+    required this.bgColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: getProportionateScreenWidth(15)),
+      child: Container(
+        padding: EdgeInsets.all(getProportionateScreenWidth(25)),
+        height: SizeConfig.screenHeight! / 6,
+        width: SizeConfig.screenWidth! / 2,
+        decoration: BoxDecoration(
+          color: bgColor,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Text(
+                  "$itemText ",
+                  style: TextStyle(
+                    color: AppColor.kTextDarkColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: getProportionateScreenWidth(25),
+                  ),
+                ),
+                Text(
+                  timeText,
+                  style: TextStyle(
+                    fontSize: getProportionateScreenWidth(20),
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              subText,
+              style: TextStyle(
+                color: AppColor.kTextDarkColor,
+                fontWeight: FontWeight.w500,
+                fontSize: getProportionateScreenWidth(15),
+                height: 1.5,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
