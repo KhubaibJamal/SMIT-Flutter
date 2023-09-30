3ec8daa6225a55fd1d384a23dd32ad3b923a2331
Khubaib Jamal <khubaib.j18@gmail.com>
2023-09-30 23:07:29 +0500

    setup project

---
+++ signin_signup_form/lib/const.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const scaffoldBgColor = Color(0xFF8C5CB3);
+const blackColor = Color(0xFF000000);
+const whiteColor = Color(0xFFFFFFFF);
+const textFieldBgColor = Color(0xFFD9D9D9);
---
+++ signin_signup_form/lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'const.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    scaffoldBackgroundColor: scaffoldBgColor,
+  );
+}
