015956984953bc1aaa025a5f437bcf038a2c2ef7
Khubaib Jamal <khubaib.j18@gmail.com>
2023-10-05 23:04:30 +0500

    sign in page

--- signin_signup_form/lib/screens/sign in/component/body.dart
+++ signin_signup_form/lib/screens/sign in/component/body.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
+import '../../../component/continue_with_text.dart';
+import '../../../component/google_button.dart';
+import '../../../const.dart';
+import '../../../size_config.dart';
+
 class Body extends StatelessWidget {
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [],
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: SizeConfig.screenHeight! * 0.04),
+
+            // log in text
+            Text(
+              "Log in to Shh!",
+              style: defaultTextStyle.copyWith(
+                  fontSize: getProportionateScreenWidth(30)),
+            ),
+
+            // google button
+            SizedBox(height: getProportionateScreenWidth(15)),
+            SizedBox(
+              width: SizeConfig.screenWidth! * 0.8,
+              child: GoogleButton(
+                text: "Log in with Google",
+                press: () {},
+              ),
+            ),
+
+            // continue with email
+            SizedBox(height: getProportionateScreenWidth(15)),
+            const ContinueWithText(text: "or log in with Email"),
+          ],
+        ),
+      ),
     );
   }
 }
