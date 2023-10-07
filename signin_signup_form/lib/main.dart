dec8b37955cefed1f888206b56cb6a608b0676f6
Khubaib Jamal <khubaib.j18@gmail.com>
2023-10-07 15:16:19 +0500

    setup firebase

--- signin_signup_form/lib/main.dart
+++ signin_signup_form/lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:signin_signup_form/route.dart';
 import 'package:signin_signup_form/screens/welcome/welcome_screen.dart';
 import 'package:signin_signup_form/theme.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
